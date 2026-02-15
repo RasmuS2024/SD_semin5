09c3a951eb37a0cbe4995f06f670c24d8ec32e09
RasmuS2024 <rasburkhanov@yandex.ru>
2026-02-15 19:01:39 +0300

    Added comments in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,58 +22,72 @@ public class Main {
         System.out.println("СОЗДАНИЕ СПЕЦИАЛИЗИРОВАННЫХ СПУТНИКОВ: \uD83D\uDEF0\uFE0F");
         printSeparator();
 
+        //создаем спутники связи
         CommunicationSatellite commSat1 = new CommunicationSatellite("Связь-1", 0.85, 500);
         CommunicationSatellite commSat2 = new CommunicationSatellite("Связь-2", 0.75, 1000);
 
+        //создаем спутники дистанционного зондирования Земли
         ImagingSatellite imgSat1 = new ImagingSatellite("ДЗЗ-1", 0.92, 2.5);
         ImagingSatellite imgSat2 = new ImagingSatellite("ДЗЗ-2", 0.45, 1.0);
         ImagingSatellite imgSat3 = new ImagingSatellite("ДЗЗ-3", 0.15, 0.5);
 
+        //создаем спутниковую группировку "Орбита-1"
         printSeparator();
         spaceOperationCenterService.createAndSaveConstellation("Орбита-1");
         printSeparator();
 
+        //создаем спутниковую группировку "Орбита-2"
         spaceOperationCenterService.createAndSaveConstellation("Орбита-2");
         printSeparator();
 
         System.out.println("ФОРМИРОВАНИЕ ГРУППИРОВОК \uD83D\uDEF0\uFE0F \uD83D\uDEF0\uFE0F \uD83D\uDEF0\uFE0F");
         printSeparator();
 
+        //добавляем спутники в группировку "Орбита-1"
         spaceOperationCenterService.addSatelliteToConstellation("Орбита-1", commSat1);
         spaceOperationCenterService.addSatelliteToConstellation("Орбита-1", imgSat1);
         spaceOperationCenterService.addSatelliteToConstellation("Орбита-1", imgSat2);
 
+        //добавляем спутники в группировку "Орбита-2"
         spaceOperationCenterService.addSatelliteToConstellation("Орбита-2", commSat2);
         spaceOperationCenterService.addSatelliteToConstellation("Орбита-2", imgSat3);
 
+        //выводим статусы группировки "Орбита-1"
         printSeparator();
         spaceOperationCenterService.showConstellationStatus("Орбита-1");
         printSeparator();
 
+        //выводим статусы группировки "Орбита-2"
         spaceOperationCenterService.showConstellationStatus("Орбита-2");
         printSeparator();
-
 
         System.out.println("АКТИВАЦИЯ СПУТНИКОВ \uD83D\uDCE1 \uD83D\uDEF0\uFE0F");
 
+        //активируем группировку "Орбита-1"
         spaceOperationCenterService.activateAllSatellites("Орбита-1");
         printSeparator();
 
+        //выводим статусы группировки "Орбита-1"
         spaceOperationCenterService.showConstellationStatus("Орбита-1");
         printSeparator();
 
+        //активируем группировку "Орбита-2"
         spaceOperationCenterService.activateAllSatellites("Орбита-2");
         printSeparator();
 
+        //выводим статусы группировки "Орбита-2"
         spaceOperationCenterService.showConstellationStatus("Орбита-2");
         printSeparator();
 
+        //запускаем миссии группировки "Орбита-1"
         spaceOperationCenterService.executeConstellationMission("Орбита-1");
         printSeparator();
 
+        //запускаем миссии группировки "Орбита-2"
         spaceOperationCenterService.executeConstellationMission("Орбита-2");
         printSeparator();
 
+        //выводим содержимое репозитория в консоль
         spaceOperationCenterService.printConstellationRepositoryToConsole();
         printSeparator();
     }
